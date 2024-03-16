c5df9727a438fa2faf4a47f6b05ab70a24530f83
Callum Askew <154433436+Fizzchimp@users.noreply.github.com>
2024-03-16 19:41:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+bool check(int arr[], int len) 
+{
+  for (int i = 0; i < len - 1; i++) 
+  {
+    if (arr[i] > arr[i + 1]) 
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+void shuffle(int arr[], int len) 
+{
+  for (int i = 0; i < len; i++)
+    {
+    int sub = arr[i];
+    int index = rand() % len;
+    arr[i] = arr[index];
+    arr[index] = sub;
+  }
+}
+
+int main() 
+{
+  srand(time(0));
+  int nums[] = {40, 54, 67, 78, 24, 2, 876};
+  int len = sizeof(nums) / 4;
+  long long int count = 0;
+
+  while (check(nums, len)) 
+  {
+    count++;
+    shuffle(nums, len);
+
+    if (count % 10000000 == 0) 
+    {
+      std::cout << count / 1000000 << " million\n";
+    }
+  }
+
+  for (int x : nums) 
+  {
+    std::cout << x << ", ";
+  }
+  std::cout << std::endl << count << " iterations";
+}
